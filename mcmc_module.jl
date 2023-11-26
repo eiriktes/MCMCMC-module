699dd71b9cb4115d72cce989367371561d4eb68c
eiriktes <150675803+eiriktes@users.noreply.github.com>
2023-11-26 15:11:28 +0100

    method version

--- mcmc_module.jl
+++ mcmc_module.jl
@@ -1,11 +1,13 @@
 module MCMC_ET
 
 # export, using, import statements are usually here; we discuss these below
-export mcmcmc_init, mcmcmc_iters, loglikelyhood, param_fonc, expand_grid
+export mcmc_glm_pois, mcmc_glm_binom, mcmc_matr_mod, mcmcmc_init, mcmcmc_iters, loglikelyhood, param_fonc, thinning, save_mc_model, load_mc_model
 
-include("helper functions.jl")
+include("lookuptables.jl")
 include("proba_distrib.jl")
 include("mc_distrib.jl")
 include("mcmcmc.jl")
+include("helper functions.jl")
+include("save models.jl")
 
 end
